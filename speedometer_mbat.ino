ec3799fb48bf2df41b31bc81b1d198f203b0e5b4
borghies <61188917+borghies@users.noreply.github.com>
2020-04-22 19:54:17 +0200

    Add files via upload

--- speedometer_mbat.ino
+++ speedometer_mbat.ino
@@ -1,10 +1,16 @@
 //#include <Zumo32U4.h>
+#include <EEPROM.h>
 #include "Zumo32U4Dummy.h"
 #include "math.h"
-#include "wire.h"
+//#include "wire.h"
+Zumo32U4ButtonA buttonA;
+Zumo32U4ButtonB buttonB;
 Zumo32U4Motors motors;
 Zumo32U4LCD lcd;
 Zumo32U4Encoders encoders(&motors);  
+
+int yellowLed = 13;
+int redLed = 17;
  
 unsigned long last_time = 0;
 unsigned long last_ten_sec = 0;
@@ -16,6 +22,8 @@ unsigned long one_minute = 60000;
 int left_speed = 400; 
 int right_speed = 400;
 
+int account_balance = EEPROM.read(0);
+
 float motorSpeed = (left_speed + right_speed) / 2;
 float batteryLevel = 100;
  
@@ -26,30 +34,14 @@ float meters_per_minute = 0;
 
 float maxSpeed = 0; 
 
-int batteryCalc(float batteryLevel, float motorSpeed)
-{ 
-  const int batteryDrain = 10;
-  const int topSpeed = 400; 
 
-  float actBatteryDrain = (motorSpeed / topSpeed)* batteryDrain; 
-
-  if (fmod(meters, 1.0) >= 0.0 && fmod(meters, 1.0) < 0.5) 
-  { 
-    if (batteryLevel > 0) 
-    { 
-      batteryLevel -= actBatteryDrain; 
-      if (batteryLevel <= 0) 
-      { 
-        batteryLevel = 0; 
-      }
-    }
-  }
-
-  return batteryLevel;
-}
 
 
 void setup() {  
+
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  
   Serial.begin(9600);
   motors.setSpeeds(left_speed, right_speed);
 }
@@ -83,7 +75,7 @@ void loop() {
     lcd.display();
 
     last_time = millis(); 
-  }
+  
 
           //BatteryLevelCode: 
     //last_ten_sec = millis(); 
@@ -100,7 +92,8 @@ void loop() {
       
     
       last_ten_sec = millis();
-    }
+    
+    //End of batteryLevelCode.
       
 // if funksjon som går inn hvert minutt
   if((millis()-last_minute) >= one_minute)
@@ -124,33 +117,127 @@ void loop() {
     maxSpeed = 0;
   
     last_minute = millis(); 
+    }
+   }
+  }  
+if (batteryLevel > 10) 
+  { 
+// digitalWrite(yellowLed, LOW);
+// digitalWrite(redLed, HIGH); 
+ ledYellow(0);
+ ledRed(0);
+ ledGreen(1);
   }
 
-    
-  if (batteryLevel > 10) 
-  { 
-    ledRed(0); 
-    ledYellow(0); 
-    ledGreen(1); 
-  }
-  if else (batteryLevel > 5)
+else if (batteryLevel > 5)
   {
-    ledRed(0); 
-    ledYellow(1); 
-    ledGreen(0); 
+// digitalWrite(yellowLed, HIGH);
+// digitalWrite(redLed, HIGH);
+ ledYellow(1);
+ ledRed(0);
+ ledGreen(0);
   }
-  if else (batteryLevel < 0) 
+
+else if (batteryLevel > 0) 
   { 
-    ledRed(1); 
-    ledYellow(0); 
-    ledGreen(0); 
+// digitalWrite(yellowLed, LOW);
+// digitalWrite(redLed, LOW);
+ ledYellow(0);
+ ledRed(1);
+ ledGreen(0);
+ 
   }
-  else (batteryLevel == 0 )
+else if (batteryLevel == 0 )
   { 
     motors.setSpeeds(0 , 0); 
-    ledRed(0); 
-    ledYellow(0); 
-    ledGreen(0); 
+//  digitalWrite(yellowLed, LOW);
+//  digitalWrite(redLed, HIGH);
+ ledYellow(0);
+ ledRed(0);
+ ledGreen(0);
+
+  
+   if (buttonA.getSingleDebouncedPress()) //lademodus
+   { 
+    while(buttonB.getSingleDebouncedPress() == false) 
+    {
+      if (account_balance > 0) 
+      { 
+        lcd.clear();
+        lcd.gotoXY(0,0);
+        lcd.write("A-1kr+10"); 
+        lcd.gotoXY(0,1);
+        lcd.write("% B:Exit"); 
+        lcd.display();
+        delay(2000);
+        if (buttonA.getSingleDebouncedPress() 
+        { 
+          batteryLevel += 10; 
+          account_balance -= 1; 
+          EEPROM.update(0,account_balance);
+  
+          lcd.clear();
+          lcd.gotoXY(0,0);
+          lcd.write(String(batteryLevel) + "%");
+          lcd.gotoXY(4,0);
+          lcd.write(String(charging_cost)+"kr");  
+          lcd.gotoXY(0,1);
+          lcd.write(String(account_balance) + "kr"); 
+          lcd.display();
+          delay(5000); 
+        }
+      }
+      else
+      { 
+      lcd.clear();
+      lcd.gotoXY(0,0);
+      lcd.write("Konto"); 
+      lcd.gotoXY(0,1);
+      lcd.write("er tom"); 
+      lcd.display();
+      delay(2000); 
+      }
+    }
   }
-      
+  
+  if (buttonB.getSingleDebouncedPress()){ //Pengemodus
+  {
+    while (buttonB.getSingleDebouncedPress() == false) 
+    { 
+      lcd.clear();
+      lcd.gotoXY(0,0);
+      lcd.write("A:" + String(account_balance) + "+5"); 
+      lcd.gotoXY(0,1);
+      lcd.write("B: Exit"); 
+      lcd.display();
+
+      if (buttonA.getSingleDebouncedPress())
+      {
+        account_balance += 5; 
+        EEPROM.update(0,account_balance);
+      }
+    }
+  }
+
+
+int batteryCalc(float batteryLevel, float motorSpeed)
+{ 
+  const int batteryDrain = 10;
+  const int topSpeed = 400; 
+
+  float actBatteryDrain = (motorSpeed / topSpeed)* batteryDrain; 
+
+  if (fmod(meters, 1.0) >= 0.0 && fmod(meters, 1.0) < 0.5) 
+  { 
+    if (batteryLevel > 0) 
+    { 
+      batteryLevel -= actBatteryDrain; 
+      if (batteryLevel <= 0) 
+      { 
+        batteryLevel = 0; 
+      }
+    }
+  }
+
+  return batteryLevel;
 }
