8c8d9efdc8b0daecede7d05ad2db46f5c4fed107
Khuongh <60898006+Khuongh@users.noreply.github.com>
2020-04-29 22:54:40 +0200

    Update Test_sammensmelt.ino

--- Slutt produkt prosjekt/Test_sammensmelt/Test_sammensmelt.ino
+++ Slutt produkt prosjekt/Test_sammensmelt/Test_sammensmelt.ino
@@ -13,6 +13,16 @@ Zumo32U4Buzzer buzzer;
 L3G gyro;
 Zumo32U4ProximitySensors proxSensors;
 
+const char fugue[] PROGMEM =
+  "! O5 L16 agafaea dac+adaea fa<aa<bac#a dac#adaea f"
+  "O6 dcd<b-d<ad<g d<f+d<gd<ad<b- d<dd<ed<f+d<g d<f+d<gd<ad"
+  "L8 MS <b-d<b-d MLe-<ge-<g MSc<ac<a ML d<fd<f O5 MS b-gb-g"
+  "ML >c#e>c#e MS afaf ML gc#gc# MS fdfd ML e<b-e<b-"
+  "O6 L16ragafaea dac#adaea fa<aa<bac#a dac#adaea faeadaca"
+  "<b-acadg<b-g egdgcg<b-g <ag<b-gcf<af dfcf<b-f<af"
+  "<gf<af<b-e<ge c#e<b-e<ae<ge <fe<ge<ad<fd"
+  "O5 e>ee>ef>df>d b->c#b->c#a>df>d e>ee>ef>df>d"
+  "e>d>c#>db>d>c#b >c#agaegfe f O6 dc#dfdc#<b c#4";
 
 
 //Creating int value for sensvaluer
